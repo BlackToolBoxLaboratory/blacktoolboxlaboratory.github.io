860c3c7d0cdcddf5fb4de8fb29c47463c27a974a
vannoel <vannoel0628@gmail.com>
2020-01-15 23:06:05 +0800

    update

--- javascript/plugins/prototype-languages.js
+++ javascript/plugins/prototype-languages.js
@@ -1,16 +1,15 @@
-window.BTBLang = new globalThis['blacktoolbox-prototype-languages'].default;
+window.BTBLang = {
+  class: globalThis['blacktoolbox-prototype-languages'].default,
+  langLoader: langLoader
+}
 
 const config = [
   { index: 'en_US', label: 'English', dictionary: {} },
   { index: 'zh_TW', label: '繁體中文', dictionary: {} },
 ];
 
-config.forEach(function(entry){
-  entry.dictionary = loadLanguage(entry.index);
-})
-
-function loadLanguage(currentlang) {
-  let obj = new XMLHttpRequest();
+function langLoader(currentlang, callbackFn) {
+  var obj = new XMLHttpRequest();
   obj.overrideMimeType('application/json');
   obj.open('GET', location.origin + location.pathname +'/languages/'+ currentlang +'.json', true);
   obj.onreadystatechange = function () {
@@ -18,12 +17,20 @@ function loadLanguage(currentlang) {
         && ('200' == obj.status)
     )
     {
-      return obj.responseText;
+      for(var i=0;i<config.length;i++) {
+        if(config[i].index === currentlang)
+        {
+          config[i].dictionary = JSON.parse(obj.responseText);
+          break;
+        }
+      }
+      if(typeof callbackFn == 'function') {
+        callbackFn();
+      }
+      return JSON.parse(obj.responseText);
     }
   };
   obj.send();
 }
 
 globalThis['blacktoolbox-prototype-languages'].initializer(config);
-
-console.log(BTBLang.translate('test'))
