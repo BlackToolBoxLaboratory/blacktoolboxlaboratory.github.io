54035bdf7fb3a99bf3129aab52e36921ee7a6e04
vannoel <vannoel0628@gmail.com>
2020-05-10 02:21:50 +0800

    update

---
+++ vue/js/chunk-2d0a40d9.ea3f0ce5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a40d9"],{"0597":function(n,e,t){"use strict";t.r(e);var r=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("router-view")},u=[],a={name:"btb-pkg-list-container"},c=a,l=t("2877"),i=Object(l["a"])(c,r,u,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0a40d9.ea3f0ce5.js.map
