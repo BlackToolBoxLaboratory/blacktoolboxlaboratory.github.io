5f22339f13c467a41df09f013eb112ae8df8da20
vannoel <vannoel0628@gmail.com>
2020-06-14 19:16:39 +0800

    update

--- react/v2/vendors.js
+++ react/v2/vendors.js
@@ -77,7 +77,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/react/";
+/******/ 	__webpack_require__.p = "/react/v2/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
